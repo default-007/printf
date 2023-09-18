6452f0e212d23c891c2cb1b24fa3d30d1ebb3d0a
Brian Otieno <brianokola@gmail.com>
2023-09-18 14:02:32 +0000

    putchar function

--- _printf.c
+++ _printf.c
@@ -15,6 +15,17 @@ int _putchar(char c)
 }
 
 /**
+ * print_char - Prints character
+ * @list: list of arguments
+ * Return: Will return the amount of characters printed.
+ */
+int print_char(va_list list)
+{
+	_write_char(va_arg(list, int));
+	return (1);
+}
+
+/**
  * print_string - Prints a string
  * @list: list of arguments
  * Return: Will return the amount of characters printed.
