76247a6855ea179d1ff2dd4ef3bfffc55c6e8b20
Brian Otieno <brianokola@gmail.com>
2023-09-18 13:59:33 +0000

    Change print string function

--- _printf.c
+++ _printf.c
@@ -15,20 +15,21 @@ int _putchar(char c)
 }
 
 /**
- * print_string - prints an array of string characters
- * @str: the variable to print
- *
+ * print_string - Prints a string
+ * @list: list of arguments
+ * Return: Will return the amount of characters printed.
  */
 
-int print_string(char *str)
+int print_string(va_list list)
 {
-	int i = 0;
+	int i;
+	char *str;
 
-	while (str[i] != '\0')
-	{
-		_putchar(str[i]);
-		i++;
-	}
+	str = va_arg(list, char *);
+	if (str == NULL)
+		str = "(null)";
+	for (i = 0; str[i] != '\0'; i++)
+		_write_char(str[i]);
 	return (i);
 }
 
